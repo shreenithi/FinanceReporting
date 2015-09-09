1447eec517a890970081f96a40ef31b63962000d
shreenithi <shreenithi20@gmail.com>
2015-09-09 15:18:11 +0530

    Update Global.asax.cs
    Moved Profile pull from Login Controller(Reporting) to Global

--- FinReport/Global.asax.cs
+++ FinReport/Global.asax.cs
@@ -4,15 +4,26 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using FinReport.Models;
 
 namespace FinReport
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        public static System.Timers.Timer timer = new System.Timers.Timer(7200000); // This will raise the event every two hours.
         protected void Application_Start()
         {
+            timer.Enabled = true;
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(timer_Elapsed);
+            
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
+        static void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            user userObj = new user();
+            userObj.populatingDb();
+        }
+
     }
 }
